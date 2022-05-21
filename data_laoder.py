ea12835a7119ef430a8b73ab295b6698663e874c
Dugeri James <dugeri.james@gmail.com>
2022-05-21 21:50:31 +0100

    commit

--- data_laoder.py
+++ data_laoder.py
@@ -5,10 +5,6 @@ from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 import torch.nn as nn
 
-
-from data_laoder import trn_densenet_features, trn_inception_features, trn_resnet_features, trn_labels
-from data_laoder import val_resnet_features, val_inception_features, val_densenet_features, val_labels
-
 from models import my_resnet, my_inception, my_densenet
 
 data_transform = transforms.Compose([
@@ -24,26 +20,6 @@ val_dset = ImageFolder('dogsandcats/valid/', transform=data_transform)
 train_loader = DataLoader(train_dset, batch_size=32, shuffle=False, num_workers=3)
 val_loader = DataLoader(val_dset, batch_size=32, shuffle=False, num_workers=3)
 
-
-class FeaturesDataset(Dataset):
-    def __init__(self, featlst1, featlst2, featlst3, labellst):
-        super(FeaturesDataset, self).__init__()
-        self.featlst1 = featlst1
-        self.featlst2 = featlst2
-        self.featlst3 = featlst3
-        self.labellst = labellst
-
-    def __getitem__(self, index):
-        return self.featlst1[index], self.featlst2[index], self.featlst3[index], self.labellst[index]
-
-    def __len__(self):
-        return len(self.labellst)
-
-trn_feat_dset = FeaturesDataset(trn_resnet_features, trn_inception_features.features, trn_densenet_features, trn_labels)
-val_feat_dset = FeaturesDataset(val_resnet_features, val_inception_features.features, val_densenet_features, val_labels)
-
-trn_feat_loader = DataLoader(trn_feat_dset, batch_size=64, shuffle=True)
-val_feat_loader = DataLoader(val_feat_dset, batch_size=64)
 
 class LayerActivations():
     features=[]
@@ -106,3 +82,23 @@ for d, la in val_loader:
     o = o.view(o.size(0), -1)
 
     val_densenet_features.extend(o.cpu().data)
+
+class FeaturesDataset(Dataset):
+    def __init__(self, featlst1, featlst2, featlst3, labellst):
+        super(FeaturesDataset, self).__init__()
+        self.featlst1 = featlst1
+        self.featlst2 = featlst2
+        self.featlst3 = featlst3
+        self.labellst = labellst
+
+    def __getitem__(self, index):
+        return self.featlst1[index], self.featlst2[index], self.featlst3[index], self.labellst[index]
+
+    def __len__(self):
+        return len(self.labellst)
+
+trn_feat_dset = FeaturesDataset(trn_resnet_features, trn_inception_features.features, trn_densenet_features, trn_labels)
+val_feat_dset = FeaturesDataset(val_resnet_features, val_inception_features.features, val_densenet_features, val_labels)
+
+trn_feat_loader = DataLoader(trn_feat_dset, batch_size=64, shuffle=True)
+val_feat_loader = DataLoader(val_feat_dset, batch_size=64)
