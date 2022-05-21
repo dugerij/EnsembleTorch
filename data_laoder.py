b6dd1fe908725d0500bfcf2524c732b931efcaa8
Dugeri James <dugeri.james@gmail.com>
2022-05-21 21:45:28 +0100

    fixed import

--- data_laoder.py
+++ data_laoder.py
@@ -1,10 +1,15 @@
+from tkinter import Variable
 import torch
 from torchvision.datasets import ImageFolder
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
+import torch.nn as nn
+
 
 from data_preprocess import trn_densenet_features, trn_inception_features, trn_resnet_features, trn_labels
 from data_preprocess import val_resnet_features, val_inception_features, val_densenet_features, val_labels
+
+from models import my_resnet, my_inception, my_densenet
 
 data_transform = transforms.Compose([
     transforms.Resize((299, 299)),
@@ -39,3 +44,65 @@ val_feat_dset = FeaturesDataset(val_resnet_features, val_inception_features.feat
 
 trn_feat_loader = DataLoader(trn_feat_dset, batch_size=64, shuffle=True)
 val_feat_loader = DataLoader(val_feat_dset, batch_size=64)
+
+class LayerActivations():
+    features=[]
+
+    def __init__(self, model) -> None:
+        self.features = []
+        self.hook = model.register_forwward_hook(self.hook_fn)
+
+    def hook_fn(self, module, input, output):
+        self.features.extend(output.view(output.size(0), -1).cpu().data)
+
+    def remove(self):
+        self.hook.remove()
+
+### For ResNet
+
+
+trn_labels = []
+trn_resnet_features = []
+for d, la in train_loader:
+    o = my_resnet(Variable(d.cuda()))
+    o = o.view(o.size(0), -1)
+    trn_labels.extend(la)
+    trn_resnet_features.extend(o.cpu().data)
+val_labels = []
+val_resnet_features = []
+for d, la in val_loader:
+    o = my_resnet(Variable(d.cuda()))
+    o = o.view(o.size(0), -1)
+    val_labels.extend(la)
+    val_resnet_features.extend(o.cpu().data)
+
+### For Inception
+
+trn_inception_features = LayerActivations(my_inception.Mixed_7c)
+for da, la in train_loader:
+    _ = my_inception(Variable(da.cuda()))
+
+trn_inception_features.remove()
+
+val_inception_features = LayerActivations(my_inception.Mixed_7c)
+for da, la in val_loader:
+    _ = my_inception(Variable(da.cuda()))
+
+val_inception_features.remove()
+
+
+### For DenseNet
+
+trn_densenet_features = []
+for d, la in train_loader:
+    o = my_densenet(Variable(d.cuda()))
+    o = o.view(o.size(0), -1)
+
+    trn_densenet_features.extend(o.cpu().data)
+
+val_densenet_features = []
+for d, la in val_loader:
+    o = my_densenet(Variable(d.cuda()))
+    o = o.view(o.size(0), -1)
+
+    val_densenet_features.extend(o.cpu().data)
--- data_preprocess.py
+++ data_preprocess.py
@@ -1,68 +1,68 @@
-from tkinter import Variable
-import torch
-import torch.nn as nn
+# from tkinter import Variable
+# import torch
+# import torch.nn as nn
 
-from data_laoder import train_loader, val_loader
-from models import my_resnet, my_inception, my_densenet
+# from data_laoder import train_loader, val_loader
+# from models import my_resnet, my_inception, my_densenet
 
-class LayerActivations():
-    features=[]
+# class LayerActivations():
+#     features=[]
 
-    def __init__(self, model) -> None:
-        self.features = []
-        self.hook = model.register_forwward_hook(self.hook_fn)
+#     def __init__(self, model) -> None:
+#         self.features = []
+#         self.hook = model.register_forwward_hook(self.hook_fn)
 
-    def hook_fn(self, module, input, output):
-        self.features.extend(output.view(output.size(0), -1).cpu().data)
+#     def hook_fn(self, module, input, output):
+#         self.features.extend(output.view(output.size(0), -1).cpu().data)
 
-    def remove(self):
-        self.hook.remove()
+#     def remove(self):
+#         self.hook.remove()
 
-### For ResNet
-
-
-trn_labels = []
-trn_resnet_features = []
-for d, la in train_loader:
-    o = my_resnet(Variable(d.cuda()))
-    o = o.view(o.size(0), -1)
-    trn_labels.extend(la)
-    trn_resnet_features.extend(o.cpu().data)
-val_labels = []
-val_resnet_features = []
-for d, la in val_loader:
-    o = my_resnet(Variable(d.cuda()))
-    o = o.view(o.size(0), -1)
-    val_labels.extend(la)
-    val_resnet_features.extend(o.cpu().data)
-
-### For Inception
-
-trn_inception_features = LayerActivations(my_inception.Mixed_7c)
-for da, la in train_loader:
-    _ = my_inception(Variable(da.cuda()))
-
-trn_inception_features.remove()
-
-val_inception_features = LayerActivations(my_inception.Mixed_7c)
-for da, la in val_loader:
-    _ = my_inception(Variable(da.cuda()))
-
-val_inception_features.remove()
+# ### For ResNet
 
 
-### For DenseNet
+# trn_labels = []
+# trn_resnet_features = []
+# for d, la in train_loader:
+#     o = my_resnet(Variable(d.cuda()))
+#     o = o.view(o.size(0), -1)
+#     trn_labels.extend(la)
+#     trn_resnet_features.extend(o.cpu().data)
+# val_labels = []
+# val_resnet_features = []
+# for d, la in val_loader:
+#     o = my_resnet(Variable(d.cuda()))
+#     o = o.view(o.size(0), -1)
+#     val_labels.extend(la)
+#     val_resnet_features.extend(o.cpu().data)
 
-trn_densenet_features = []
-for d, la in train_loader:
-    o = my_densenet(Variable(d.cuda()))
-    o = o.view(o.size(0), -1)
+# ### For Inception
 
-    trn_densenet_features.extend(o.cpu().data)
+# trn_inception_features = LayerActivations(my_inception.Mixed_7c)
+# for da, la in train_loader:
+#     _ = my_inception(Variable(da.cuda()))
 
-val_densenet_features = []
-for d, la in val_loader:
-    o = my_densenet(Variable(d.cuda()))
-    o = o.view(o.size(0), -1)
+# trn_inception_features.remove()
 
-    val_densenet_features.extend(o.cpu().data)
+# val_inception_features = LayerActivations(my_inception.Mixed_7c)
+# for da, la in val_loader:
+#     _ = my_inception(Variable(da.cuda()))
+
+# val_inception_features.remove()
+
+
+# ### For DenseNet
+
+# trn_densenet_features = []
+# for d, la in train_loader:
+#     o = my_densenet(Variable(d.cuda()))
+#     o = o.view(o.size(0), -1)
+
+#     trn_densenet_features.extend(o.cpu().data)
+
+# val_densenet_features = []
+# for d, la in val_loader:
+#     o = my_densenet(Variable(d.cuda()))
+#     o = o.view(o.size(0), -1)
+
+#     val_densenet_features.extend(o.cpu().data)
