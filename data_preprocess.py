368534fc3f7664d9b03ccdc224a4685b4cfdd7ea
Dugeri James <dugeri.james@gmail.com>
2022-05-21 18:02:02 +0100

    preprocess data for each model

--- data_preprocess.py
+++ data_preprocess.py
@@ -23,5 +23,30 @@ for d, la in val_loader:
 ### For Inception
 
 trn_inception_features = LayerActivations(my_inception.Mixed_7c)
+for da, la in train_loader:
+    _ = my_inception(Variable(da.cuda()))
+
+trn_inception_features.remove()
+
+val_inception_features = LayerActivations(my_inception.Mixed_7c)
+for da, la in val_loader:
+    - = my_inception(Variable(da.cuda()))
+
+val_inception_features.remove()
+
 
 ### For DenseNet
+
+trn_densenet_features = []
+for d, la in train_loader:
+    o = my_densenet(Variable(d.cuda()))
+    o = o.view(o.size(0), -1)
+
+    trn_densenet_features.extend(o.cpu().data)
+
+val_densenet_features = []
+for d, la in val_loader:
+    o = my_densenet(Variable(d.cuda()))
+    o = o.view(o.size(0), -1)
+
+    val_densenet_features.extend(o.cpu().data)
